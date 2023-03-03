a00cbe4084eadd3ba5b87c4e890bbf579cdb7a79
SerhiiKutovyi <wagenrex@gmail.com>
2023-03-03 14:37:29 +0200

    listContacts

--- contacts.js
+++ contacts.js
@@ -1,24 +1,13 @@
-const path = require("path");
-const contactsPath = path.join(__dirname, "./db/contacts.json");
+const fs = require('node:fs/promises');
+const path = require('node:path');
 
-const fs = require("fs/promises");
-// fs.readFile(contactsPath, "utf-8")
-//   .then((data) => console.log(data))
-//   .catch((error) => console.log(error.message));
+const contactsPath = path.join(__dirname, './db/contacts.json');
 
 // TODO: задокументировать каждую функцию
 
-async function listContacts({ action }) {
-  switch (action) {
-    case "read":
-      const list = await fs.readFile(contactsPath, "utf8");
-      console.log(list);
-
-      break;
-
-    default:
-      console.log("Unknown action");
-  }
+async function listContacts() {
+  const list = await fs.readFile(contactsPath, 'utf8');
+  return list;
 }
 
 function getContactById(contactId) {
@@ -33,6 +22,6 @@ function addContact(name, email, phone) {
   console.log();
 }
 
-// module.export = listContacts;
-
-listContacts({ action: "read" });
+module.exports = {
+  listContacts,
+};
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const contacts = require('./contacts');
 
+async function getAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      const ContactsList = await contacts.listContacts();
+      console.log(ContactsList);
+      break;
+
+    case 'get':
+      break;
+
+    case 'add':
+      break;
+
+    case 'remove':
+      break;
+
+    default:
+      break;
+  }
+}
+
+getAction({ action: 'list' });
